883bb2d61bb1ae802ff41d298024f37d5c6a40b6
Rakesh Mothukuri <rockey5520@gmail.com>
2020-06-22 17:37:35 +0100

    incremental commit

---
+++ Problems/Out of bounds!/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class FixingStringIndexOutOfBoundsException {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String string = scanner.nextLine();
+    int index = scanner.nextInt();
+    if (index < 0 || index >= string.length()) {
+      System.out.println("Out of bounds!");
+    } else {
+      System.out.println(string.charAt(index));
+    }
+  }
+}
---
+++ Problems/Throw a checked exception/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+public class Main {
+
+  public static void method() throws IOException {
+    throw new Zero
+  }
+
+  /* Do not change code below */
+  public static void main(String[] args) {
+    try {
+      method();
+    } catch (RuntimeException e) {
+      System.out.println("RuntimeException");
+    } catch (Exception e) {
+      System.out.println("Exception");
+    }
+  }
+}
